7f2d72c3319fa53c5ba37ec05145ed90c88b2f90
let-if <letifylkal6@gmail.com>
2026-04-26 00:48:19 -0700

    clean repository - reset project structure

--- backend/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
-
-require_once "routes/api.php";
